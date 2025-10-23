4dd9233202ec634cfc883831510b78fdeca71027
Zairaxx <brandontsegai@gmail.com>
2025-10-23 13:57:15 +0200

    added solution to uppgift 2

--- index.js
+++ index.js
@@ -1,22 +1,59 @@
-const greetBtn = document.querySelector("#greetBtn");
+let addTodoBtn = document.querySelector("#addTodo");
+let taskInput = document.querySelector("#task");
+let todoList = document.querySelector("#todo-list");
 
-const nameInput = document.querySelector("#name");
 
-greetBtn.addEventListener("click", (event) => {
-    let h2 = document.createElement("h2");
-    h2.innerText = "Welcome " + nameInput.value;
+addTodoBtn.addEventListener("click", () => {
+    
+    if(todoList.children.length < 5){
+        let li = document.createElement("li");
+        li.innerText = taskInput.value;
 
-    document.body.append(h2);
+        //delete-btn
+        let deleteBtn = document.createElement("button");
+        deleteBtn.innerText = "Ta bort";
+        li.append(deleteBtn);
 
+        //delete-funktion
+        deleteBtn.addEventListener("click", (e) => {
+            //ta bort tillhörande li-tagg;
+            e.target.parentElement.remove();
+        })
+        todoList.append(li);
+    } else {
+        alert("Maxgräns nådd!")
+    }
 })
 
-//DOM Traversal
+//Uppgift 2
+let number1 = document.querySelector("#tal1");
+let number2 = document.querySelector("#tal2");
+let addBtn = document.querySelector("#add");
+let subBtn = document.querySelector("#subtract");
+let multiBtn = document.querySelector("#multiply")
+let answer = document.querySelector("#answer");
 
-let allBtns = document.querySelectorAll("button");
+const calculate = (operator) => {
+    //Alternativ 2
+    answer.innerText = eval(`${number1.value}${operator}${number2.value}`);
+}
 
-allBtns.forEach(btn => {
-    btn.addEventListener("click", (event) => {
-        let h2 = event.target.previousElementSibling;
-        h2.style.color = "lightgreen";
-    })
-})
+addBtn.addEventListener("click", () => calculate("+"));
+subBtn.addEventListener("click", () => calculate("-"));
+multiBtn.addEventListener("click", () => calculate("*"));
+
+//Uppgift 3
+
+let number = document.querySelector("#number");
+
+number.addEventListener("change", (e) => {
+    if(number.value < 4){
+        e.target.style.color = "red";
+    } else if (number.value <= 7){
+    e.target.style.color = "orange";}
+    else if (number.value <= 10){
+        e.target.style.color = "green";
+    } else {
+        alert(`You wrote ${number.value}. Pick a number between 1-10`)
+        e.target.style.color = "black";
+}})
